f4cf4f06b415cf34b09a73661c2e9202fb0aa3c7
Vadershik <gogofight2k20@gmail.com>
2023-11-24 23:25:47 +0300

    debug+new functions

--- Logic.py
+++ Logic.py
@@ -1,5 +1,6 @@
 from settings import MINPLAYERS, MAXPLAYERS, MINDICE, MAXDICE, BOARDSIZE
 from Parser import Parser
+from typing import Optional, Union
 
 import logging
 import random
@@ -21,26 +22,45 @@ class Game:
 
     def getBoard(self):
         """Print a game board."""
+        
         for i in range(len(self.board)):
             print(self.board[i])
         logging.info("Maked a request to show board.")
 
     def movePlayer(self, numPlayer: int, startIndex: int, endIndex: int):
         """Function to move a player in board."""
-        assert MINPLAYERS <= numPlayer <= MAXPLAYERS
+        
+        assert MINPLAYERS-1 <= numPlayer <= MAXPLAYERS
         assert 0 <= startIndex <= 11
         assert 0 <= endIndex <= 11
+        
         self.board[startIndex].remove(numPlayer)
         self.board[endIndex].append(numPlayer)
         logging.info(f"{numPlayer} moved from {startIndex+1} to {endIndex+1}")
 
+        #Checking owner of this place
+        if self.getOwnerOfPos(endIndex)==0:
+            logging.info(f"{numPlayer} get new place {endIndex+1}")
+            self.setNewOwnerOfPos(numPlayer, endIndex)
+        else:
+            logging.info(f"{numPlayer} get in place of a {self.getOwnerOfPos(endIndex)} player.")
+            self.getTask(numPlayer)
+
+
     def getTask(self, numPlayer: int):
+        """Function to give a random task from tasks.txt"""
+
         parser = Parser()
         logging.info(f"{numPlayer} get task")
+
+        task = parser.getTask()
+        print(task)
+        
         return parser.getTask()
 
     def getPosPlayer(self, numPlayer: int):
         """Function to get position of a player."""
+
         for i in range(len(self.board)):
             if numPlayer in self.board[i]:
                 return i
@@ -48,20 +68,37 @@ class Game:
 
     def getNewPosPlayer(self, currentIndex: int) -> int:
         """Function to make a roll dice."""
+        
         #Formula: (currentIndex+random(1,6))%12
         #CurrentIndex start from 0 to 11
+        
         newPos = ((currentIndex)+random.randint(MINDICE, MAXDICE))%12
+        
         assert self.checkPosPlayer(newPos)
+        
         return newPos
 
-    def checkPosPlayer(self, position) -> bool:
+    def checkPosPlayer(self, position: int) -> bool:
         return 0 <= position <= 11
 
-    def getOwnerOfPos(self, position) -> int:
+    def getOwnerOfPos(self, position: int) -> int:
         return self.ownersBoard[position]
     
     def setNewOwnerOfPos(self, numPlayer: int, position : int):
         self.ownersBoard[position] = numPlayer
+
+    def removePlayer(self, numPlayer: int, killer: Union[int, None] = None):
+        #removing from board
+        position = self.checkPosPlayer(numPlayer)
+        self.board[position].remove(numPlayer)
+
+        if killer!=None:
+            for i in range(len(self.ownersBoard)):
+                if self.ownersBoard[i]==numPlayer:
+                    self.ownersBoard[i]=killer
+                logging.info(f"{killer} get all places by {numPlayer}")
+        logging.info(f"{numPlayer} be removed")
+
 
 
 def startMenu():
@@ -73,11 +110,14 @@ def startMenu():
         print("INPUTERROR: Players need to be integer value")
         sys.exit(0)
     assert MINPLAYERS <= players <= MAXPLAYERS
-    board = [[] for _ in range(BOARDSIZE)]
-    a = Game(board)
+    a = Game()
     a.getBoard()
     a.setPlayersOnBoard(players)
     a.movePlayer(2, 0, a.getNewPosPlayer(a.getPosPlayer(2)))
+    a.setNewOwnerOfPos(2, a.getPosPlayer(2))
+    a.movePlayer(1, 0, a.getPosPlayer(2))
+    print()
+    print(a.getOwnerOfPos(a.getPosPlayer(1)))
     a.getBoard()
 
 
